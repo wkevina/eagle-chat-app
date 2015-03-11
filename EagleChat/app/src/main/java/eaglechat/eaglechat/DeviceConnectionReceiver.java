4edca4146cfb9d0c275dd40bc34f339ea2f650bb
Kevin Ward <antiomiae@gmail.com>
2015-03-10 23:01:12 -0500

    Renamed some stuff, added a service. Doesn't work yet.

--- EagleChat/app/src/main/java/eaglechat/eaglechat/DeviceConnectionReceiver.java
+++ EagleChat/app/src/main/java/eaglechat/eaglechat/DeviceConnectionReceiver.java
@@ -8,22 +8,28 @@ import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.util.Log;
 
-public class DeviceAttachedReceiver extends BroadcastReceiver {
+import com.hoho.android.usbserial.driver.UsbId;
+
+public class DeviceConnectionReceiver extends BroadcastReceiver {
     private static final String TAG = "eaglechat.eaglechat";
 
     private static final String ACTION_USB_PERMISSION =
             "eaglechat.eaglechat.USB_PERMISSION";
-
-    public DeviceAttachedReceiver() {
+    private Context mContext;
+    public DeviceConnectionReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        mContext = context;
         String action = intent.getAction();
+        Log.d(TAG, action);
+
+        UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        Log.d(TAG, "Device: " + device);
+
         if (ACTION_USB_PERMISSION.equals(action)) {
             synchronized (this) {
-                UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-
                 if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                     if (device != null) {
                         notifyDeviceAttached(device);
@@ -35,8 +41,16 @@ public class DeviceAttachedReceiver extends BroadcastReceiver {
         } else {
             if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
                 UsbManager manager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
-                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                // filter devices
+                boolean isEagleChat = device.getProductId() == UsbId.EAGLE_CHAT && device.getVendorId() == UsbId.VENDOR_ATMEL;
+                Log.d(TAG, "Is EagleChat device: " + isEagleChat);
+                if (!isEagleChat) {
+                    Log.d(TAG, "Not an EagleChat device.");
+                    return;
+                }
+
                 if (!manager.hasPermission(device)) { // if we do not have permission to access this device
+                    Log.d(TAG, "Requesting permission to access device.");
                     final PendingIntent permissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
                     manager.requestPermission(device, permissionIntent); // request it!
                 } else {
@@ -50,6 +64,9 @@ public class DeviceAttachedReceiver extends BroadcastReceiver {
 
     private void notifyDeviceAttached(UsbDevice device) {
         Log.d(TAG, "Device attached: " + device);
+        //Intent intent = new Intent(mContext, EagleChatCommService.class);
+        //intent.putExtra(UsbManager.EXTRA_DEVICE, device);
+        //mContext.startService(intent);
     }
 
     private void notifyDeviceDetached() {
