e2426b25071a299a96dcb84c4954d3b851b2441f
Kevin Ward <antiomiae@gmail.com>
2015-04-28 10:56:37 -0500

    Fixed things.

--- EagleChat/app/src/main/java/eaglechat/eaglechat/MainActivity.java
+++ EagleChat/app/src/main/java/eaglechat/eaglechat/MainActivity.java
@@ -356,6 +356,12 @@ public class MainActivity extends PeregrineActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        if (id == R.id.action_burn) {
+            Util.burn(this);
+            finish();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
